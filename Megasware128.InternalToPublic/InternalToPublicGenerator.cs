4573ecd5bb7305869851fae42ad98ce546ee0291
Jordy Hulck <Jordy.Hulck@outlook.com>
2022-01-10 10:59:13 +0100

    Add project files.

---
+++ Megasware128.InternalToPublic/InternalToPublicGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+
+namespace Megasware128.InternalToPublic
+{
+    [Generator]
+    public class InternalToPublicGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
